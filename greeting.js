d439973c34c69a75aa7fb2645c86d46f1c2bb0ed
Ebunoluwa <ebuneohwodo@gmail.com>
2023-05-20 21:45:47 +0100

    first commit

---
+++ greeting.js
@@ -0,0 +1,13 @@
+export const informalGreeting = (person) => {
+    console.log(`Hi, ${person} Okezie`);
+}
+
+export const formalGreeting = (person) => {
+    console.log(`Hello, ${person} Okezie`); 
+}
+
+const greeting = (name) => {
+    console.log(`Howdy! ${name}`)
+}
+
+export default greeting;
